91bd20c4c79c3324881a23d2d8eb6d3ec66f859a
huan <huandualeo@gmail.com>
2021-10-06 15:08:18 +0700

    css form admin

--- src/main/resources/static/assets/admin/order/order-ctrl.js
+++ src/main/resources/static/assets/admin/order/order-ctrl.js
@@ -64,7 +64,7 @@ app.controller("order-ctrl", function($scope, $http) {
 
 	$scope.pager = {
 		page: 0,
-		size: 10,
+		size: 5,
 		get items() {
 			var start = this.page * this.size;
 			return $scope.items.slice(start, start + this.size);
